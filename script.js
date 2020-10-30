0d8e09eac6654a2ea18e561af274d846519a015e
Johan Lindstrom <johan.lindstrom@swisslog.com>
2020-10-30 07:50:53 -0600

    first piece of code pushed

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const balanceEl = document.querySelector('.balance .value');
+const incomeTotalEl = document.querySelector('.outcome-total');
+const outcomeTotalEl = document.querySelector('income-total');
+const chartEl = document.querySelector('.chart');
+
+//Dashboard buttons
+const expenseBtn = document.querySelector('.tab1');
+const incomeBtn = document.querySelector('.tab2');
+const allBtn = document.querySelector('.tab3');
+const exspenseEl = document.querySelector('#expense');
+const incomeEl = document.querySelector('#income');
+const allEl = document.querySelector('#all');
